98ed5294d3b3efffb31e8729e24d2be16fd5ca1f
tamil1706 <tamilarasan380150@gmail.com>
2025-07-17 11:50:57 +0530

    freelancer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "Tamil",
           from_email: form.email,
-          to_email: "tamilarasan380150@gmail.com",
+          to_email: "papupapu1706@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
